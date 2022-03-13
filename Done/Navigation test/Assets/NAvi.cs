a33134be967ffc45e9f64653330072a4a99031a0
StaceyXC <982753946@qq.com>
2022-03-13 10:43:57 +0800

    New test on swarm simulation

--- Done/Navigation test/Assets/NAvi.cs
+++ Done/Navigation test/Assets/NAvi.cs
@@ -6,16 +6,24 @@ using UnityEngine.AI;
 public class NAvi : MonoBehaviour
 {
     public Transform target;
+    public float speed;
+    NavMeshAgent m_agent;
     // Start is called before the first frame update
     void Start()
     {
         if (target != null)
-            this.gameObject.GetComponent<NavMeshAgent>().destination=target.position;
+            {
+                m_agent=gameObject.GetComponent<NavMeshAgent>();
+            }
+  
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_agent.destination=target.transform.position;
+       // m_agent.transform.position = Vector3.MoveTowards(this.transform.position,m_agent.nextPosition,speed * Time.deltaTime);
+      //  transform.position = Vector3.MoveTowards(start.position, end.position, speed * Time.deltaTime);
     }
 }
---
+++ Test/Swarm-Flocking-Unity3D-master/README.md
@@ -0,0 +1,10 @@
+# Swarm Flocking Simulation in Unity3D
+
+<img src="misc/Swarm-Flocking.gif" />
+
+## Usage:
+- Make sure you have Unity3D installed on your system
+- Go to the Assets/Scenes directory and open the scene file (let Unity3D take care of the rest)
+
+### Note:
+Run after building the project for best performance
